b7fc53be62aba8098e5e669f603c0b45f12948ba
Andy Zhang <zhangandy94@gmail.com>
2016-11-17 20:58:08 -0500

    Some fixxes

--- a5/AutomaticSignal.h
+++ a5/AutomaticSignal.h
@@ -1,6 +1,6 @@
 // Declare a uCondition
 #define AUTOMATIC_SIGNAL uCondition cond;
 // if pred is true, then run before, wait, then after. Otherwise, wake up everyone waiting on uCondition
-#define WAITUNTIL(pred, before, after) if (!(pred)) { before; do { cond.wait(); } while (!(pred)); after; }
+#define WAITUNTIL(pred, before, after) if (!(pred)) { before; while (!cond.empty()) { cond.signal(); } do { cond.wait(); } while (!(pred)); after; }
 // Return the expression
 #define RETURN(expr) while (!cond.empty()) { cond.signal(); } return expr;
